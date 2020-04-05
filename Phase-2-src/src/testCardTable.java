c7136075c63ce4349450cba0438d75e43c4ce473
apainintheneck <apainintheneck@gmail.com>
2020-04-04 21:33:43 -0700

    Added more test to testCardTable.java.

--- Phase-2-src/src/testCardTable.java
+++ Phase-2-src/src/testCardTable.java
@@ -12,6 +12,7 @@ public class testCardTable
       myCardTable.setLocationRelativeTo(null);
       myCardTable.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       
+      //Create Jlabels
       JLabel card1, card2, computer, human;
       card1 = new JLabel("Card #1");
       card1.setHorizontalAlignment(JLabel.CENTER);
@@ -22,6 +23,7 @@ public class testCardTable
       human = new JLabel("Human");
       human.setHorizontalAlignment(JLabel.CENTER);
       
+      //Add jlabels to play area
       myCardTable.pn1PlayArea.add(card1);
       myCardTable.pn1PlayArea.add(card2);
       myCardTable.pn1PlayArea.add(computer);
@@ -29,6 +31,38 @@ public class testCardTable
 
       // show everything to the user
       myCardTable.setVisible(true);
+      
+      //Pause Program
+      try
+      {
+         Thread.sleep(1000);
+      } catch (InterruptedException e)
+      {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      
+      //Reset card1 and card2 labels
+      card1.setText("");
+      card2.setText("");
+      //Repaint jpanel
+      myCardTable.pn1PlayArea.repaint();
+      
+      //Pause Program
+      try
+      {
+         Thread.sleep(1000);
+      } catch (InterruptedException e)
+      {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      
+      //Reset card1 and card2 labels
+      card1.setText("Card #3");
+      card2.setText("Card #4");
+      //Repaint jpanel
+      myCardTable.pn1PlayArea.repaint();
    }
 
 }
