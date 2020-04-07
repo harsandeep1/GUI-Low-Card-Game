e1b44ce2148d998cd4302f9a348d1ff58c3ba359
apainintheneck <apainintheneck@gmail.com>
2020-04-06 18:21:43 -0700

    Fixed problems in assig5 main so that everything runs now.

--- Phase-2-src/src/assig5.java
+++ Phase-2-src/src/assig5.java
@@ -13,24 +13,39 @@ public class assig5
    
    public static void main(String[] args)
    {  
+      GUICard cardIcons = new GUICard();
+      
       // establish main frame in which program will run
       CardTable myCardTable 
          = new CardTable("CardTable", NUM_CARDS_PER_HAND, NUM_PLAYERS);
       myCardTable.setSize(800, 600);
       myCardTable.setLocationRelativeTo(null);
       myCardTable.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-      // show everything to the user
-      myCardTable.setVisible(true);
-
+      
       // CREATE LABELS ----------------------------------------------------
+      //For computer hand and human hand
       for(int i = 0; i < NUM_CARDS_PER_HAND; i++)
       {
-         computerLabels[i] = new JLabel(GUICard.getIcon(new Card(Card.valuRanks[i])));
+         computerLabels[i] = new JLabel(GUICard.getBackCardIcon());
          humanLabels[i] = new JLabel(GUICard.getIcon(new Card(Card.valuRanks[i])));
       }
       
-  
+      //For play area
+      JLabel computer, human, score;
+      computer = new JLabel("Computer");
+      computer.setIcon(GUICard.getIcon(new Card('K', Card.Suit.DIAMONDS)));
+      computer.setHorizontalAlignment(JLabel.CENTER);
+      computer.setVerticalTextPosition(SwingConstants.BOTTOM);
+      computer.setHorizontalTextPosition(SwingConstants.CENTER);
+      human = new JLabel("Human");
+      human.setIcon(GUICard.getIcon(new Card('2', Card.Suit.HEARTS)));
+      human.setHorizontalAlignment(JLabel.CENTER);
+      human.setVerticalTextPosition(SwingConstants.BOTTOM);
+      human.setHorizontalTextPosition(SwingConstants.CENTER);
+      score = new JLabel();
+      score.setHorizontalAlignment(JLabel.CENTER);
+      score.setText("Computer wins!");
+      
       // ADD LABELS TO PANELS -----------------------------------------
       // First add cards to both players hands
       for(int i = 0; i < NUM_CARDS_PER_HAND; i++)
@@ -38,27 +53,11 @@ public class assig5
          myCardTable.pn1ComputerHand.add(computerLabels[i]);
          myCardTable.pn1HumanHand.add(humanLabels[i]);
       }
-      
-      // and two random cards in the play region (simulating a computer/hum ply)
-      JLabel computer, human, score;
-      computer = new JLabel("Computer");
-      computer.setIcon(GUICard.getIcon(new Card('K')));
-      computer.setHorizontalAlignment(JLabel.CENTER);
-      computer.setVerticalTextPosition(SwingConstants.BOTTOM);
-      computer.setHorizontalTextPosition(SwingConstants.CENTER);
-      human = new JLabel("Human");
-      human.setIcon(GUICard.getIcon(new Card('2')));
-      human.setHorizontalAlignment(JLabel.CENTER);
-      human.setVerticalTextPosition(SwingConstants.BOTTOM);
-      human.setHorizontalTextPosition(SwingConstants.CENTER);
-      score = new JLabel();
-      score.setHorizontalAlignment(JLabel.CENTER);
-      score.setText("Computer wins!");
       //Add jlabels to play area
       myCardTable.pn1PlayArea.add(computer);
       myCardTable.pn1PlayArea.add(score);
       myCardTable.pn1PlayArea.add(human);
-
+      
       // show everything to the user
       myCardTable.setVisible(true);
    }
